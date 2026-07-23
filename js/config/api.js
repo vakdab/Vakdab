cd8e493ede72f5e43d5882ce702dce396bc47c99
vakdab <vakdabpro@gmail.com>
2026-07-23 14:23:31 +0300

    Update api.js

--- js/config/api.js
+++ js/config/api.js
@@ -1,7 +1,2 @@
-// ===== КОНФІГУРАЦІЯ ПРОКСІ ТА API =====
-// Оригінальні рядки: L5675-L5681
-
-export const PROXY_URL = 'https://monoanime.animegran8.workers.dev';
-export const CLOUDINARY_CLOUD_NAME = 'w1a1sqp0';
-export const CLOUDINARY_UPLOAD_PRESET = 'VakDab';
-export const ANIMEUA_BASE = 'https://animeua.club';
+// Порожній файл – зарезервовано для майбутніх API-адрес
+export default {};
