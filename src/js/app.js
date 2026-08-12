18c94d29d7f4332e5e58d205a84253d8d9d3ba5b
vakdab <vakdabpro@gmail.com>
2026-08-12 07:41:21 +0300

    Refresh anime relation and metadata logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=player-fix-7';
+import './app-legacy.js?v=anime-page-v4';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
