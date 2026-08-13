5c643226198f65577dd069aa4aa271f3fe9ac38f
vakdab <vakdabpro@gmail.com>
2026-08-13 09:26:42 +0300

    Add vertical Популярні section to home page (same popular-card style, no carousel)

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=video-controls-2';
+import './app-legacy.js?v=home-popular-1';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
