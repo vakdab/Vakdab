46eb5cffc311956d0ca53a8dbc4469d7fc4db985
VakDab Maintainer <maintainer@vakdab.local>
2026-08-14 11:16:12 +0000

    bust cache for sticker profile update

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=profile-media-2';
+import './app-legacy.js?v=stickers-profile-v1';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
