810e43914e3905efb06a479d41874feb19b59e13
vakdab <3.0796115e+08+vakdab@users.noreply.github.com>
2026-08-16 06:59:12 +0000

    Refresh Mikai player module cache

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=hikka-mikai-v1';
+import './app-legacy.js?v=hikka-mikai-v2';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
