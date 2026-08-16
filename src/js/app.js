ee113c5a5c5cdcf90e425bc3bd6f77254c755354
vakdab <3.0796115e+08+vakdab@users.noreply.github.com>
2026-08-16 10:46:02 +0000

    Refresh catalog UI assets

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=mikai-schedule-v3';
+import './app-legacy.js?v=catalog-ui-v1';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
