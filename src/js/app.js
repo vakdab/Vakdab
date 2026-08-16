3162d6a1ee7bbfdb5759f5dd6b7134d9b4cf511d
vakdab <3.0796115e+08+vakdab@users.noreply.github.com>
2026-08-16 10:10:31 +0000

    Keep schedule access in catalog controls

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=schedule-theme-v1';
+import './app-legacy.js?v=release-menu-v2';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
