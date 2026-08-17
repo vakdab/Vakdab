56e371228fad0493a00c26cf69b4940079a0c701
vakdab <3.0796115e+08+vakdab@users.noreply.github.com>
2026-08-17 07:24:12 +0000

    Add community post interactions and media

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=20260817-community-hierarchy-v2';
+import './app-legacy.js?v=20260817-community-features-v1';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
