cee9564754364daf9cd5d35a93657191cf68601f
vakdab <3.0796115e+08+vakdab@users.noreply.github.com>
2026-08-17 10:46:24 +0000

    Bust stale catalog module cache

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /** VakDab browser entrypoint. */
-import './app-legacy.js?v=20260817-reader-optimization-v2';
+import './app-legacy.js?v=20260817-catalog-fix-v3';
 import './utils/dom.js';
 import './utils/string.js';
 import './utils/time.js';
