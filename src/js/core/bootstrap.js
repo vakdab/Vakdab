8138e3374d84971949db27ad6d32ae23cb3e448b
vakdab <vakdabpro@gmail.com>
2026-09-02 10:53:56 +0300

    chore: bump cache version to genre-rail-v2

--- src/js/core/bootstrap.js
+++ src/js/core/bootstrap.js
@@ -16,7 +16,7 @@ export function bootstrap() {
         }
     });
     startGlobalEvents();
-    bootstrapPromise = import('../legacy/app-legacy.js?v=20260902-genre-rail-v1')
+    bootstrapPromise = import('../legacy/app-legacy.js?v=20260902-genre-rail-v2')
         .then(module => {
             window.VakDabLegacy = module;
             // Expose compatibility services only after the legacy module has fully evaluated.
