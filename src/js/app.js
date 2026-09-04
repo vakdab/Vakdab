e744ae5371c7651c14bd3f5efb911e599e11cc07
vakdab <vakdabpro@gmail.com>
2026-09-04 08:58:04 +0300

    chore: bump bootstrap import version

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { bootstrap } from './core/bootstrap.js?v=20260904-profile-tasks-v1';
+import { bootstrap } from './core/bootstrap.js?v=20260904-profile-tasks-v2';
 
 const telegramStartParam = globalThis.Telegram?.WebApp?.initDataUnsafe?.start_param || '';
 if (telegramStartParam === 'live' && window.location.hash.slice(1) !== 'live') window.location.hash = 'live';
