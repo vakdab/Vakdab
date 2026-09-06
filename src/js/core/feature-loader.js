0455d130321685c92b3ecd29e18d9dc6ec959a36
vakdab <vakdabpro@gmail.com>
2026-09-06 17:01:03 +0300

    refactor: remove novel feature loader

--- src/js/core/feature-loader.js
+++ src/js/core/feature-loader.js
@@ -1,6 +1,5 @@
 const loaders = Object.freeze({
     manga: () => import('../components/manga/reader.js?v=20260824-settings-redesign-v1'),
-    novel: () => import('../components/novel/reader.js?v=20260824-settings-redesign-v1'),
     player: () => import('../pages/player/animePage.js?v=20260824-settings-redesign-v1'),
     profile: () => import('../pages/profile/profile.js?v=20260824-settings-redesign-v1'),
     stickers: () => import('../pages/profile/stickersPage.js?v=20260824-settings-redesign-v1')
