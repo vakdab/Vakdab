7fe06b052a869b3ff56563b7194d7f3f708fadc2
vakdab <vakdabpro@gmail.com>
2026-09-06 17:01:13 +0300

    chore: bump homeLegacy.js import version (remove-ranobe-v1)

--- src/js/pages/profile/stickersLegacy.js
+++ src/js/pages/profile/stickersLegacy.js
@@ -2,7 +2,7 @@ import { Storage } from '../../core/compat/storage.js?v=20260824-settings-redesi
 import { db } from '../../services/firebase/client.js';
 import { Router } from '../../core/compat/router.js?v=20260901-home-recs-v3';
 import { PROFILE_STICKER_SLOTS, getDefaultStickers, showToast, showToastProgress, escapeHtml, removeStickerBackground } from '../../legacy/app-legacy.js?v=20260905-stickers-sync-v1';
-import { uploadBlobToCloudinary } from '../home/homeLegacy.js?v=20260829-vertical-catalog-28-v1';
+import { uploadBlobToCloudinary } from '../home/homeLegacy.js?v=20260906-remove-ranobe-v1';
 
         function stickerFaceSvg(variant) {
             const s = 'stroke="currentColor" stroke-width="1.6" fill="none" stroke-linecap="round" stroke-linejoin="round"';
