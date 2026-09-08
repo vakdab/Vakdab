b51376602610229723a8cb2623d16478cac24f13
vakdab <vakdabpro@gmail.com>
2026-09-08 08:45:32 +0300

    chore: bump bootstrap.js import version

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { bootstrap } from './core/bootstrap.js?v=20260908-merged-search-v1';
+import { bootstrap } from './core/bootstrap.js?v=20260908-inline-search-v1';
 
 const telegramStartParam = globalThis.Telegram?.WebApp?.initDataUnsafe?.start_param || '';
 if (telegramStartParam === 'live' && window.location.hash.slice(1) !== 'live') window.location.hash = 'live';
