1dba6ec1199367e2d25f9bbd41a38d709e36ef9c
vakdab <vakdabpro@gmail.com>
2026-09-09 22:24:02 +0300

    fix: rebind opening skip to active video node

--- src/js/core/compat/storage.js
+++ src/js/core/compat/storage.js
@@ -1,5 +1,5 @@
 import { Auth } from './auth.js';
-import { PROFILE_STICKER_SLOTS, getDefaultStickers } from '../../legacy/app-legacy.js?v=20260909-player-v7';
+import { PROFILE_STICKER_SLOTS, getDefaultStickers } from '../../legacy/app-legacy.js?v=20260909-player-v8';
         export const Storage = {
             _syncTimer: null,
             _pendingSyncScope: null,
