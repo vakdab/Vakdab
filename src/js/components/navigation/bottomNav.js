d8b645192c35d89bbb4e48444266da8c6e67aad1
vakdab <vakdabpro@gmail.com>
2026-09-09 20:36:45 +0300

    fix: make anime player and opening skip reliable

--- src/js/components/navigation/bottomNav.js
+++ src/js/components/navigation/bottomNav.js
@@ -1,6 +1,6 @@
 import { loadFeature } from '../../core/feature-loader.js';
 import { Router } from '../../core/compat/router.js?v=20260901-home-recs-v3';
-import { openPlayerPage, closePlayerPage } from '../../legacy/app-legacy.js?v=20260906-hero-fix-v5';
+import { openPlayerPage, closePlayerPage } from '../../legacy/app-legacy.js?v=20260909-player-v2';
 
 export function initBottomNav() {
 
