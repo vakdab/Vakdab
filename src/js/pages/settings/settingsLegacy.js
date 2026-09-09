c4dd6f56c18f1d5b6e47b71c36327aabd8cf130e
vakdab <vakdabpro@gmail.com>
2026-09-09 22:24:09 +0300

    fix: rebind opening skip to active video node

--- src/js/pages/settings/settingsLegacy.js
+++ src/js/pages/settings/settingsLegacy.js
@@ -4,7 +4,7 @@ import {
     editExistingProfileVideo, escapeHtml, isGifUrl, isVideoUrl,
     profileMediaMarkup, renderProfilePage, showToast,
     syncLeftdockActive, toggleTheme
-} from '../../legacy/app-legacy.js?v=20260909-player-v7';
+} from '../../legacy/app-legacy.js?v=20260909-player-v8';
 
         let settingsState = { tab: 'profile', previewOpen: true };
 
