9bce207146c0d4f269719b3b8d8dfd0c2145f599
vakdab <vakdabpro@gmail.com>
2026-09-09 20:50:19 +0300

    style: compact animated opening skip and stabilize player layout

--- src/js/core/bootstrap.js
+++ src/js/core/bootstrap.js
@@ -24,7 +24,7 @@ export function bootstrap() {
         document.body.classList.remove('android-tv-mode');
     } catch (_) {}
 
-    bootstrapPromise = import('../legacy/app-legacy.js?v=20260909-player-v2')
+    bootstrapPromise = import('../legacy/app-legacy.js?v=20260909-player-v3')
         .then(module => {
             window.VakDabLegacy = module;
             // Expose compatibility services only after the legacy module has fully evaluated.
