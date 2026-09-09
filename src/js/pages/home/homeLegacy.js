7fc402668f0ebe3fe19236428ce4d34917d2bf07
vakdab <vakdabpro@gmail.com>
2026-09-09 21:11:15 +0300

    fix: keep mobile player controls at video bottom

--- src/js/pages/home/homeLegacy.js
+++ src/js/pages/home/homeLegacy.js
@@ -3,7 +3,7 @@ import {
     Auth, Router, Storage, escapeHtml,
     loadGenrePageContent, renderProfilePage, renderSettingsPage,
     showToast, showToastProgress, syncLeftdockActive
-} from '../../legacy/app-legacy.js?v=20260909-player-v3';
+} from '../../legacy/app-legacy.js?v=20260909-player-v4';
 import { getProfile, saveProfile, getProfileDisplayName, stripNicknamePrefix } from '../settings/settingsLegacy.js?v=20260824-settings-redesign-v1';
 import { debugLog } from '../../utils/debug.js';
 import { fetchTmdbCardInfo } from '../../services/tmdb.js?v=20260824-settings-redesign-v1';
