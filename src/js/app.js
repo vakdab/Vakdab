32b4521fadddbfa3ddab293df67bc751b1708946
vakdab <3.0796115e+08+vakdab@users.noreply.github.com>
2026-09-21 17:43:54 +0000

    Revert "Add Anime and Manga menu navigation"
    This reverts commit 729e6d4e03431a8816e66040547409b7604c9ead.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { bootstrap } from './core/bootstrap.js?v=20260921-anime-manga-menu-v1';
+import { bootstrap } from './core/bootstrap.js?v=20260921-podium-medals-v2';
 
 try {
     if (globalThis.Telegram?.WebApp) {
